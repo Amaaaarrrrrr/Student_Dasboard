7527c9387a17e27ae7d76937f8ee04663a09bca4
Michael <michael.njoroge3@student.moringaschool.com>
2025-05-08 10:00:13 +0300

    login endpoint and registration endpoint

---
+++ Client/src/pages/student/Announcement.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Announcement = () => {
+  return (
+    <div>
+      <h1>Announcement</h1>
+      <p>Track your Annoucement progress here.</p>
+    </div>
+  );
+};
+
+export default Announcement;
